5930b45a5eb7c5f1b7dad0d402c6bd27e661d99e
eltonaguiar <eltonaguiar@users.noreply.github.com>
2026-05-25 21:16:29 +0000

    deploy: 0424a9362c795385b701dfeb2d8a54497433020f

--- battleground/features/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Battleground feature engineering modules."""
-
-from battleground.features.funding_rate_features import compute_funding_features
-from battleground.features.cross_sectional_momentum import compute_cross_sectional_momentum
